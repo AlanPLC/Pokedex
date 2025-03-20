f07ce1dc0e97d52ae90b611b450c3b061a5ae3d1
Alan Carrizo <alancarrizo1017@gmail.com>
2025-03-19 23:06:50 -0300

    Cambios en Layout Pokedex: Agregado de input en botón búsqueda

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,21 @@
 import { Stack } from "expo-router";
-import { View, Image, Pressable } from "react-native";
+import { View, Image, Pressable, TextInput, TouchableOpacity} from "react-native";
 import logo from "../assets/pokebola.png"
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { useState, useEffect } from 'react'
 
 
 export default function Layout() {
+
+  const [searchVisible, setSearchVisible] = useState(false)
+  const [search, setSearch] = useState("")
+
+  const handleSearch = () =>{
+    setSearchVisible(!searchVisible)
+    setSearch("")
+    console.log(searchVisible)
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <Stack
@@ -20,11 +31,35 @@ export default function Layout() {
                 />
             ),
             headerRight: () => (
-                <Pressable style={({ pressed }) => [
-                    { marginRight: 5, opacity: pressed ? 0.5 : 1, transform: [{ scale: pressed ? 0.9 : 1 }] }
-                    ]}>
-                    <AntDesign name="search1" size={24} color="black" style={{marginRight: 5}}/>
-                </Pressable>
+              <View style={{display: "flex", flexDirection:"row", alignItems:"center"}}>
+                {!searchVisible ? (
+                <TouchableOpacity 
+                onPress={handleSearch}
+                hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+                style={{margin: 0}}
+                >
+                  <AntDesign name="search1" size={30} color="black" style={{marginRight: 5}}/>
+                </TouchableOpacity>
+          
+              ):( 
+                <>
+                <TouchableOpacity 
+                onPress={handleSearch}
+                hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+                style={{margin: 0}}>
+                  <AntDesign name="close" size={30} color="black" style={{marginRight: 5}}/>
+                </TouchableOpacity>
+                <TextInput
+                placeholder="Buscar Pokémon"
+                value={search}
+                onChangeText={setSearch}
+                style={{
+                   
+                  borderBottomWidth: 1, borderBottomColor: "#000", marginBottom: 0 }}
+              />
+                </>
+              )}
+              </View>
       )}}
       />
     </View>
